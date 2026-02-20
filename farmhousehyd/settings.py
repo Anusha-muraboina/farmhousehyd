46d60ecab1308600a61b69f74c2b0f0399f8a485
anushamuraboina99 <anushamuraboina99@gmail.com>
2026-02-20 18:54:29 +0530

    updated the s3
    updated the s3

--- farmhousehyd/settings.py
+++ farmhousehyd/settings.py
@@ -298,3 +298,16 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 
 
+# s3 bucket
+
+AWS_STORAGE_BUCKET_NAME = "farmhousehyd-media"
+AWS_S3_REGION_NAME = "ap-south-1"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
+
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
+
