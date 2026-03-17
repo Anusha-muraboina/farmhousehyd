bff074ac928006684671eedbc3b830c522b207c5
anushamuraboina99 <anushamuraboina99@gmail.com>
2026-03-17 16:45:55 +0530

    updated the chnages in the settings
    updated the chnages in the settings

--- farmhousehyd/settings.py
+++ farmhousehyd/settings.py
@@ -163,7 +163,7 @@ DATABASES = {
         "NAME": "farmhouse_hyd",        # DB NAME
         "USER": "farmhouse_user",       # DB USER
         "PASSWORD": "Farmhouse@2026!",  # DB PASSWORD
-        "HOST": "3.110.215.192",            # or localhost
+        "HOST": "127.0.0.1",            # or localhost
         "PORT": "3306",
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
