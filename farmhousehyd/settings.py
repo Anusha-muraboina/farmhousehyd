802b05c1a1a1eed070b0b66a97fdb7e4a48320b8
anushamuraboina99 <anushamuraboina99@gmail.com>
2026-06-25 13:48:12 +0530

    updated the chnages in the settings
    updated the chnages in the settings

--- farmhousehyd/settings.py
+++ farmhousehyd/settings.py
@@ -28,6 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# SECRET_KEY = 'django-insecure-1@_wfmmx_cuuy!&_%a_dl*zh!_+)bpp!6k5w@#ga8c+g+#=q8^'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
